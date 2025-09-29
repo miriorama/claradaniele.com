cb4c740461994ba63db068f140bbed9d685561a5
Daniele Caraglio <daniele.caraglio@gmail.com>
2025-09-29 17:23:14 +0200

    impl: sticky

--- js/main.js
+++ js/main.js
@@ -42,7 +42,6 @@ class Clara {
 
     static async view(viewId = null) {
         document.querySelector('.lightbox-overlay')?.remove();
-        document.querySelector('.project-close')?.remove();
 
         if (viewId) {
             history.pushState({}, viewId, (viewId !== '/' ? '?' + viewId : '/'));
@@ -130,7 +129,7 @@ class Clara {
         $projectClose.className = 'project-close';
         $projectClose.setAttribute('onclick',"Clara.view('/')");
         $projectClose.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/></svg>&nbsp;&nbsp;&nbsp;Progetti'
-        $body.appendChild($projectClose);
+        $project.appendChild($projectClose);
 
         // Aggiungo l'overflow:hidden al body per evitare che interferisca con lo scroll del lavoro
         $body.classList.add('no-overflow');
